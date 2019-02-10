4540b29516ca299c7c834471801f1b13171107c5
fragkog <kfragkog@gmail.com>
2019-02-10 20:29:07 +0000

    movie page

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // example search for title http://www.omdbapi.com/?apikey=c85894a7&t=mad+max
 // example search for search http://www.omdbapi.com/?apikey=c85894a7&s=mad+max&type=movie
 
+document.getElementById("movieSearch").value = "Aliens";
+
 //vars
 let results = document.querySelector(".results");
 let searchInput = null;
@@ -33,7 +35,7 @@ function fetchSearch() {
     })
     .then(function(myJson) {
       if (myJson.Response == "True") {
-        console.log(myJson);
+        // console.log(myJson);
         myJson.Search.forEach(function(film) {
           let movie = document.createElement("div");
           movie.classList.add("movie");
@@ -50,6 +52,63 @@ function fetchSearch() {
           movie.appendChild(div2);
           movie.appendChild(div3);
           results.classList.remove("hidden");
+          movie.addEventListener("click", function() {
+            let id = film.imdbID;
+            fetch("http://www.omdbapi.com/?apikey=c85894a7&i=" + id)
+              .then(function(response) {
+                if (!response.ok) {
+                  console.error("HTTP error, status = " + response.status);
+                }
+                return response.json();
+              })
+              .then(function(movieJson) {
+                console.log(movieJson);
+                let moviePage = document.querySelector(".moviePage");
+                moviePage.classList.remove("hidden");
+                results.style.opacity = "0.05";
+                let div4 = document.createElement("div");
+                let div5 = document.createElement("div");
+                moviePage.appendChild(div4);
+                moviePage.appendChild(div5);
+                div4.classList.add("border");
+                let img2 = document.createElement("img");
+                img2.src = movieJson.Poster;
+                div5.appendChild(img2);
+                let div6 = document.createElement("div");
+                div6.innerText = movieJson.Title;
+                div6.style.fontWeight = "500";
+                div6.style.fontSize = "29px";
+                div4.appendChild(div6);
+                let div7 = document.createElement("div");
+                div7.innerText = movieJson.Director + " - " + movieJson.Year;
+                div4.appendChild(div7);
+                let div8 = document.createElement("div");
+                div8.innerText = movieJson.Actors;
+                div4.appendChild(div8);
+                let div9 = document.createElement("div");
+                div9.innerText = movieJson.Plot;
+                div4.appendChild(div9);
+                let div10 = document.createElement("div");
+                div10.innerText = movieJson.Genre;
+                div4.appendChild(div10);
+                let div11 = document.createElement("div");
+                div11.innerText =
+                  "imdb Rating: " +
+                  movieJson.imdbRating +
+                  " - " +
+                  "Metascore Rating: " +
+                  movieJson.Metascore;
+                div4.appendChild(div11);
+                let img3 = document.createElement("img");
+                img3.src = "arrowLeft.ico";
+                moviePage.appendChild(img3);
+                img3.addEventListener("click", function() {
+                  moviePage.classList.add("hidden");
+                  moviePage.innerHTML = "";
+                  results.style.opacity = "1";
+                });
+              });
+          });
         });
       } else if (myJson.Response == "False") {
         results.innerText = myJson.Error;
